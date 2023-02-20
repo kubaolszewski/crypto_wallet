ab182ccb64ecf4d5216f3bbb534cbbc2f6ae7d6f
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-02-20 12:30:53 +0100

    Build simple home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome :)',
+              style: GoogleFonts.righteous(fontSize: 32),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              child: Text(
+                'Logout',
+                style: GoogleFonts.righteous(fontSize: 32),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
