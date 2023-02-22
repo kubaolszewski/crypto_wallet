c6e9c3fb147b291cb1207173802f48055f709522
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-02-22 12:51:58 +0100

    Implement errorhandling in Dio

--- lib/data_sources/remote_data_source.dart
+++ lib/data_sources/remote_data_source.dart
@@ -4,8 +4,13 @@ class CurrencyDataSource {
   Future<Map<String, dynamic>?> getCurrencyData({
     required String currencyName,
   }) async {
-    final serverResponse = await Dio().get<Map<String, dynamic>>(
-        'http://rest.coinapi.io/v1/exchangerate/$currencyName/USD?apikey=AC91C75A-6B9B-4E11-8F80-1F123E2B216E');
-    return serverResponse.data;
+    try {
+      final serverResponse = await Dio().get<Map<String, dynamic>>(
+          'http://rest.coinapi.io/v1/exchangerate/$currencyName/USD?apikey=AC91C75A-6B9B-4E11-8F80-1F123E2B216E');
+      return serverResponse.data;
+    } on DioError catch (error) {
+      throw Exception(
+          error.response?.data['error']['message'] ?? 'Unknown error');
+    }
   }
 }
